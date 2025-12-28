f901157334ceae81ef792d610be5552bf7889929
James Rees <jrees183@gmail.com>
2025-12-28 21:32:04 +0000

    Chat database models

--- app/model/models.py
+++ app/model/models.py
@@ -17,3 +17,36 @@ class UserCreds(db.Model, UserMixin):
 class User(UserCreds, UserMixin):
     """Wrapper class to add Flask-Login functionality to UserCreds"""
     pass
+
+class UserToChat(db.Model):
+    __tablename__ = "userToChat"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("userCreds.id"), nullable=False)
+    chat_id = db.Column(db.Integer, db.ForeignKey("chat.id"), nullable=False)
+
+    def __repr__(self) -> str:
+        return f"<UserToChat id={self.id} user_id={self.user_id} chat_id={self.chat_id}>" 
+    
+class Chat(db.Model):
+    __tablename__ = "chat"
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    chat_name = db.Column(db.String(150), nullable=True)
+    chat_image = db.Column(db.LargeBinary, nullable=True)
+
+    def __repr__(self) -> str:
+        return f"<Chat id={self.id}>"
+
+class Message(db.Model):
+    __tablename__ = "message"
+
+    id = db.Column(db.Integer, primary_key=True)
+    chat_id = db.Column(db.Integer, db.ForeignKey("chat.id"), nullable=False)
+    sender_id = db.Column(db.Integer, db.ForeignKey("userCreds.id"), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, server_default=db.func.now())
+
+    def __repr__(self) -> str:
+        return f"<Message id={self.id} chat_id={self.chat_id} sender_id={self.sender_id}>"
